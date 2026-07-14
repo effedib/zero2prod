8ce3f3c533cb7131362f0e6e945a3ceaf12f5d02
cicciodb@hotmail.it <cicciodb@hotmail.it>
2026-07-14 23:04:47 +0200

    [DRAFT] username and password [skip ci]

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,7 +1,10 @@
+use argon2::{
+    Argon2, PasswordHasher,
+    password_hash::{SaltString, rand_core::OsRng},
+};
 use std::sync::LazyLock;
 
 use secrecy::SecretString;
-use sha3::Digest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
@@ -167,8 +170,11 @@ impl TestUser {
     }
 
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Argon2::default()
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
             VALUES ($1, $2, $3)",
