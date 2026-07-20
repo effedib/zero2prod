3d3056091ac7b54b6d25992508eb589b8e78251b
cicciodb@hotmail.it <cicciodb@hotmail.it>
2026-07-20 21:38:01 +0200

    login & home rendered with tera [skip ci]

--- src/routes/home/mod.rs
+++ src/routes/home/mod.rs
@@ -1,8 +1,15 @@
-use actix_web::HttpResponse;
 use actix_web::http::header::ContentType;
+use actix_web::{HttpResponse, web};
+use tera::{Context, Tera};
 
-pub async fn home() -> HttpResponse {
+pub async fn home(tera: web::Data<Tera>) -> HttpResponse {
+    let context = Context::new();
+    let rendered_html = match tera.render("home.html", &context) {
+        Ok(r) => r,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("home.html"))
+        .body(rendered_html)
 }
--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,31 @@
-use actix_web::HttpResponse;
 use actix_web::http::header::ContentType;
+use actix_web::{HttpResponse, web};
+use tera::{Context, Tera};
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>,
+}
+
+pub async fn login_form(tera: web::Data<Tera>, query: web::Query<QueryParams>) -> HttpResponse {
+    let error_html = match query.0.error {
+        None => "".to_string(),
+        Some(error_message) => error_message,
+    };
+
+    let rendered_html = match render_login_form(&tera, error_html.as_str()) {
+        Ok(html) => html,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(rendered_html)
+}
+
+pub fn render_login_form(tera: &Tera, error_html: &str) -> Result<String, tera::Error> {
+    let mut context = Context::new();
+    context.insert("error_html", error_html);
+
+    tera.render("login.html", &context)
 }
