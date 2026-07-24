060839da3a7161a7a7d3255eb77521ccdf16478e
effedib <cicciodb@hotmail.it>
2026-07-24 12:00:22 +0200

    sessions extractor

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,5 +1,60 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, http::header::ContentType, web};
+use anyhow::Context;
+use sqlx::PgPool;
+use tera::Tera;
+use uuid::Uuid;
 
-pub async fn admin_dashboard() -> HttpResponse {
-    HttpResponse::Ok().finish()
+use crate::session_state::TypedSession;
+
+fn e500<T>(e: T) -> actix_web::Error
+where
+    T: std::fmt::Debug + std::fmt::Display + 'static,
+{
+    actix_web::error::ErrorInternalServerError(e)
+}
+
+pub async fn admin_dashboard(
+    tera: web::Data<Tera>,
+    session: TypedSession,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let username = if let Some(user_id) = session.get_user_id().map_err(e500)? {
+        get_username(user_id, &pool).await.map_err(e500)?
+    } else {
+        todo!()
+    };
+
+    let rendered_html = match render_dashboard_form(&tera, username.as_str()) {
+        Ok(html) => html,
+        Err(e) => return Err(e500(e)),
+    };
+
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(rendered_html))
+}
+
+pub fn render_dashboard_form(tera: &Tera, username: &str) -> Result<String, String> {
+    let mut context = tera::Context::new();
+    context.insert("username", username);
+
+    tera.render("dashboard.html", &context)
+        .map_err(|_| "error while trying to render the dashboard html".into())
+}
+
+#[tracing::instrument(name = "Get username", skip(pool))]
+async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+    SELECT username
+    FROM users
+    WHERE user_id = $1
+    "#,
+        user_id
+    )
+    .fetch_one(pool)
+    .await
+    .context("Failed to perform a query to retrieve a username")?;
+
+    Ok(row.username)
 }
