0cf3a4b32ffa65a4d08688d9f97e678416e56c6a
cicciodb@hotmail.it <cicciodb@hotmail.it>
2026-07-31 23:46:27 +0200

    [DRAFT] worker loop [skip ci]

--- src/issue_delivery_worker.rs
+++ src/issue_delivery_worker.rs
@@ -1,8 +1,29 @@
+use std::time::Duration;
+
 use sqlx::{Executor, PgPool, Postgres, Transaction};
 use tracing::{Span, field::display};
 use uuid::Uuid;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
+
+async fn worker_loop(pool: &PgPool, email_client: &EmailClient) -> Result<(), anyhow::Error> {
+    loop {
+        match try_execute_task(pool, email_client).await {
+            Ok(ExecutionOutcome::EmptyQueue) => {
+                tokio::time::sleep(Duration::from_secs(10)).await;
+            }
+            Err(_) => {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+            Ok(ExecutionOutcome::TaskCompleted) => {}
+        }
+    }
+}
+
+enum ExecutionOutcome {
+    TaskCompleted,
+    EmptyQueue,
+}
 
 #[tracing::instrument(
     skip_all,
@@ -12,41 +33,47 @@ use crate::{domain::SubscriberEmail, email_client::EmailClient};
     ),
     err
 )]
-async fn try_execute_task(pool: &PgPool, email_client: &EmailClient) -> Result<(), anyhow::Error> {
-    if let Some((transaction, issue_id, email)) = dequeue_task(pool).await? {
-        Span::current()
-            .record("newsletter_issue_id", display(issue_id))
-            .record("subscriber_email", display(&email));
-        match SubscriberEmail::parse(email.clone()) {
-            Ok(email) => {
-                let issue = get_issue(pool, issue_id).await?;
-                if let Err(e) = email_client
-                    .send_email(
-                        &email,
-                        &issue.title,
-                        &issue.html_content,
-                        &issue.text_content,
-                    )
-                    .await
-                {
-                    tracing::error!(
-                        error.cause_chain = ?e,
-                            error.message = %e,
-                            "Failed to deliver issue to a confirmed subscriber. Skipping"
-                    );
-                }
-            }
-            Err(e) => {
+async fn try_execute_task(
+    pool: &PgPool,
+    email_client: &EmailClient,
+) -> Result<ExecutionOutcome, anyhow::Error> {
+    let task = dequeue_task(pool).await?;
+    if task.is_none() {
+        return Ok(ExecutionOutcome::EmptyQueue);
+    }
+    let (transaction, issue_id, email) = task.unwrap();
+    Span::current()
+        .record("newsletter_issue_id", display(issue_id))
+        .record("subscriber_email", display(&email));
+    match SubscriberEmail::parse(email.clone()) {
+        Ok(email) => {
+            let issue = get_issue(pool, issue_id).await?;
+            if let Err(e) = email_client
+                .send_email(
+                    &email,
+                    &issue.title,
+                    &issue.html_content,
+                    &issue.text_content,
+                )
+                .await
+            {
                 tracing::error!(
                     error.cause_chain = ?e,
                         error.message = %e,
-                        "Skipping a confirmed subscriber. Their stored contact details are invalid"
+                        "Failed to deliver issue to a confirmed subscriber. Skipping"
                 );
             }
         }
-        delete_task(transaction, issue_id, &email).await?;
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                    error.message = %e,
+                    "Skipping a confirmed subscriber. Their stored contact details are invalid"
+            );
+        }
     }
-    Ok(())
+    delete_task(transaction, issue_id, &email).await?;
+    Ok(ExecutionOutcome::TaskCompleted)
 }
 
 type PgTransaction = Transaction<'static, Postgres>;
@@ -56,7 +83,7 @@ async fn dequeue_task(
     pool: &PgPool,
 ) -> Result<Option<(PgTransaction, Uuid, String)>, anyhow::Error> {
     let mut transaction = pool.begin().await?;
-    let query = sqlx::query!(
+    let r = sqlx::query!(
         r#"
         SELECT newsletter_issue_id, subscriber_email
         FROM issue_delivery_queue
@@ -64,8 +91,9 @@ async fn dequeue_task(
         SKIP LOCKED
         LIMIT 1
         "#,
-    );
-    let r = transaction.fetch_optional(query).await?;
+    )
+    .fetch_optional(&mut *transaction)
+    .await?;
     if let Some(r) = r {
         Ok(Some((
             transaction,
